670517c111e6bd2a4119463fbf37cebba4108448
Roger Huba <rogerhuba@gmail.com>
2020-07-28 21:04:58 -0700

    demo complete

--- class-18/cipher/cipher/cipher.py
+++ class-18/cipher/cipher/cipher.py
@@ -9,7 +9,7 @@ def encrypt(plain_text, key):
     --> 34567
     """
     encrypted_text = ''
-
+    print(f'The plain text data is {plain_text}.')
     for char in plain_text:
         num = int(char)
         shifted_number = num + key
@@ -18,12 +18,31 @@ def encrypt(plain_text, key):
         # if shifted_number > 9:
         #     shifted_number -= 10
         encrypted_text += str(shifted_number)
-    print(encrypted_text)    
+    print(f'The encrypted data with a shift of {key} is {encrypted_text}.')
+    print('#'*50)   
     return encrypted_text
 
 
+def decrypt(encoded, key):
+    return encrypt(encoded, -key)
+
 if __name__ == "__main__":
-    assert encrypt('12345', 2) == '34567'
-    assert encrypt('12345', 12) == '34567'
-    assert encrypt('12345', 21) == '23456'
-    assert encrypt('12345', 356) == '78901'
+    # assert encrypt('12345', 1) == '23456'
+    # assert encrypt('12345', 2) == '34567'
+    # assert encrypt('12345', 12) == '34567'
+    # assert encrypt('12345', 21) == '23456'
+    # assert encrypt('12345', 356) == '78901'
+    # assert encrypt('23456', -1) == '12345'
+
+ 
+    key = 5
+    plain = '12345'
+
+    encypted = encrypt(plain, key)
+    print (encypted)
+    # return --> 67890
+    decrypted = decrypt(encypted, key)
+    print(decrypted)
+    # return --> 12345
+
+    assert decrypted == plain
